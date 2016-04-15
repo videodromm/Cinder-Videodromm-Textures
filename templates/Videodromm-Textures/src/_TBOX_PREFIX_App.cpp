b427467fe85ed640f4892c61f808874dc70a13a7
Bruce LANE <bruce@batchass.fr>
2016-04-15 14:56:11 +0200

    template fix

--- templates/Videodromm-Textures/src/_TBOX_PREFIX_App.cpp
+++ templates/Videodromm-Textures/src/_TBOX_PREFIX_App.cpp
@@ -1,34 +1,58 @@
-
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-#include "cinder/gl/Texture.h"
+
+#include "VDTexture.h"
 
 using namespace ci;
 using namespace ci::app;
+using namespace std;
+using namespace VideoDromm;
 
 class _TBOX_PREFIX_App : public App {
 
 public:
 
-    void setup() override;
-    void draw() override;
-    void update() override;
-    void mouseDown(MouseEvent event) override;
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+	void cleanup() override;
+private:
+	VDTextureList				mTexs;
+	fs::path					mTexturesFilepath;
+
 
 };
 
 
 void _TBOX_PREFIX_App::setup()
 {
-
+	// initialize 
+	mTexturesFilepath = getAssetPath("") / "textures.xml";
+	if (fs::exists(mTexturesFilepath)) {
+		// load textures from file if one exists
+		mTexs = VDTexture::readSettings(loadFile(mTexturesFilepath));
+	}
+	else {
+		// otherwise create a texture from scratch
+		fs::path imagePath = getAssetPath("") / "0.jpg";
+		mTexs.push_back(TextureImage::create(imagePath.string()));
+	}
 }
 
 void _TBOX_PREFIX_App::update()
 {
 
 }
+void _TBOX_PREFIX_App::cleanup()
+{
 
+	// save warp settings
+	VDTexture::writeSettings(mTexs, writeFile(mTexturesFilepath));
+
+	quit();
+}
 void _TBOX_PREFIX_App::mouseDown(MouseEvent event)
 {
     
@@ -36,7 +60,8 @@ void _TBOX_PREFIX_App::mouseDown(MouseEvent event)
 
 void _TBOX_PREFIX_App::draw()
 {
-
+	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::draw(mTexs[0]->getTexture());
 }
 
 
