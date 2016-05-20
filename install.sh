969b31aafc0b1ccb0e69b414aef1003b2a6c9a01
Bruce LANE <bruce@batchass.fr>
2016-05-21 00:58:23 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo Install dependencies
+echo Cinder-Hap2
+git clone https://github.com/videodromm/Cinder-Hap2.git ../Cinder-Hap2
+echo Cinder-Videodromm
+git clone https://github.com/videodromm/Cinder-Videodromm ../Cinder-Videodromm
+echo Required Cinder blocks checked out
